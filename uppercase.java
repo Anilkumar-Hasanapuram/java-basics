f45d8633f231e163275722beeaec7a7c8fc2be02
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2022-12-01 12:19:07 +0530

    lower case to upper case
    Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
    
    input
    
    Hello world
    
    output
    
    
    HELLO WORLD

---
+++ uppercase.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class uppercase
+{
+    public static void main(String args[])
+    {
+        String str;
+        Scanner sc=new Scanner(System.in);
+        str=sc.nextLine();
+        System.out.println(str.toUpperCase());
+        
+    }
+}
