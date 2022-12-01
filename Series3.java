2427a30e59048e1dfaaadfb187d36316dfbc9fc6
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2022-12-01 10:57:23 +0530

    Series3
    Write a program to generate the first n terms in the series --- 3, 9, 27, 81,...,... 
    
    Input format: 
    
    The input containing an integer which denotes 'n' 
    
    Output format: 
    
    Print the series and refer the sample output for formatting
    
    Sample Input & Output:
    
    5
    
    3 9 27 81 243

---
+++ Series3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Series3
+{
+    public static void main(String[] args)
+    {
+        //Write your code
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            int i1=3;
+            int cube=(int)Math.pow(i1,i);
+            System.out.print(cube+" ");
+            
+            
+        }
+    }
+}
