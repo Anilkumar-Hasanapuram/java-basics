85b9c9e012865c620066eabc9c1100afcac3030a
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2022-12-01 14:03:52 +0530

    hello using methods

---
+++ hello.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class hello{
+  // Write a public static function `greet` that returns "hello world!"
+  public static String greet(String str)
+  nstr="" 
+  {
+      nstr=str;
+  }
+  return nstr;
+public static void main(String[] args)
+    
+    {
+        String str;
+        Scanner sc=new Scanner(System.in);
+        str=sc.nextInt();
+        
+        System.out.println(greet(str));
+       
+    }
+  
+}
