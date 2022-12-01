cf3e00c55667d8c4c7a238a754172d348fcd72fa
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2022-12-01 11:48:30 +0530

    string upper case
    Write a program to change the given string to uppercase without using string library functions: 
    
     INPUT & OUTPUT FORMAT: 
    
    Input consists of 1 string. 
    
    Output print the Uppercase of given string.
    
    Sample Input:
    cyfuno
    
    Sample Output:
    
    CYFUNO

---
+++ String7.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class String7
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String str;
+        str=sc.nextLine();
+         System.out.println(str.toUpperCase());
+    }
+}//Code here
