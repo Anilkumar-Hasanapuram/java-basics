c462908cbdf4ca60f1f5e4fcd20d4e62bd09a04b
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2022-12-01 12:29:19 +0530

    power of a number
    Write a program to find the power of a number. In order to find the power of a number, multiply the number itself up to exponent times.
    
    Example:
    Suppose, 5 is the base and 4 is the exponent. In order to find the power of a number, multiply the number itself 4 times, i.e. (5 * 5 * 5 * 5 = 625).
    
    Steps to Find Power of a Number
    Read or initialize base and exponent.
    Take another variable (power) to store the result and initialize it to 1.
    Using the for loop or while loop, multiply the base by power and store the result into power.
    Repeat the above step (3) until the exponent becomes 0.
    Print the result.
    Sample Input-1:
    5
    3
    Sample Output-1:
    125

---
+++ Powerofanumber.java
@@ -0,0 +1,13 @@
+//Code here
+import java.util.Scanner;
+public class Powerofanumber
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println((int)Math.pow(a,b));
+    }
+}
