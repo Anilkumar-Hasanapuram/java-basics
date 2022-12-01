f33252c7fb7327b25b4924642ba9c7e33ea8515a
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2022-12-01 13:55:10 +0530

    rhombus pattern
    Write a code to print the pattern dynamically
    
    Sample Input:
    
    5
    
    Sample Output: 
    
        *
    
       * *
    
      *   *
    
     *     *
    
    *       *
    
     *     *
    
      *   *
    
       * *
    
        *

---
+++ Apattern4.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Apattern4
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);  
+         int rows = sc.nextInt();
+         int i,j,k;
+    for (i=1; i<= rows ; i++)   
+    {  
+         for (j = rows; j > i ; j--)   
+         {  
+           System.out.print(" ");  
+         }  
+       System.out.print("*");  
+      for (k = 1; k < 2*(i -1) ;k++)   
+      {   
+        System.out.print(" ");   
+       }  
+     if( i==1)   
+{   
+System.out.println("");  
+}  
+else  
+{  
+System.out.println("*");   
+}  
+}   
+for (i=rows-1; i>= 1 ; i--)  
+{  
+for (j =rows; j > i ; j--)   
+{  
+System.out.print(" ");  
+}  
+System.out.print("*");  
+for (k =1; k < 2*(i -1) ;k++)   
+{  
+System.out.print(" ");  
+}  
+if(i==1)  
+System.out.println("");  
+else  
+System.out.println("*");  
+}  
+}
+}
