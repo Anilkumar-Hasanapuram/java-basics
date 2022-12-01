a4134499616ac56e29ccbafe54de939b11d82a84
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2022-12-01 11:47:46 +0530

    string concatenation
    Write a C program to find the concatenation of given two strings using string library functions:
    
    INPUT & OUTPUT FORMAT: 
    
    Input consists of 2 strings.
    
    Output consists of concatenated strings
    
    Sample Input:
    cyfuno
    
    technology
    
    Sample Output:
    
    cyfunotechnology

---
+++ String6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class String6
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str,str1;
+        str=sc.nextLine();
+        str1=sc.nextLine();
+        //System.out.println(str+str1);
+        System.out.println(str.concat(str1));
+       
+    }
+}//Code here
