4fe859ccc4577cab1cf45aefafc1fbbec535cecf
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2022-12-01 12:18:11 +0530

    sum of two numbers
    Define a function which can compute the sum of two numbers using recursion.
    
    sample input:
    
    4
    
    8
    
    Sample output:
    
    12

---
+++ sotn.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class sotn
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+    }
+}
