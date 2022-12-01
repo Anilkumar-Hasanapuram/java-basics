924128658a16304960ca16dec88860e7b5ed858a
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2022-12-01 14:03:06 +0530

    double number
    You need to double the integer and return it.
    
    Input:
    
    2
    
    output:

---
+++ duble.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class duble
+{
+    public static void main(String args[])
+    {
+        int number;
+        Scanner sc=new Scanner(System.in);
+        number=sc.nextInt();
+        System.out.println(number*2);
+    }
+}
