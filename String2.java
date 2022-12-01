8fdf9b5ee81f5bcbd29f0519a69b8468f6840765
Anil kumar <108290351+Anilkumar-Hasanapuram@users.noreply.github.com>
2022-12-01 11:44:47 +0530

    length of the string
    Write a program to find the length of the given string using string library functions:
    
    INPUT & OUTPUT FORMAT:
    
    Input consists of 1 string. 
    
    Output print the length of the string.
    
    Sample Input:
    cyfuno
    
    Sample Output:
    
    6

---
+++ String2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class String2
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        System.out.println(str.length());
+    }
+}
